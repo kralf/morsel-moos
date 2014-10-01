8608743c316b80db68f3221febfd25f3cf39c13a
Ralf Kaestner <ralf.kaestner@gmail.com>
2014-10-01 11:08:17 +0200

    Adaptated to revised Morsel API

--- python/lib/morsel_moos/client/facade.py
+++ python/lib/morsel_moos/client/facade.py
@@ -3,5 +3,4 @@ from morsel_moos.client import *
 #-------------------------------------------------------------------------------
 
 def MOOSClient(**kargs):
-  return framework.createInstance("client", type = "MOOSClient",
-    world = framework.world, **kargs)
+  return framework.createInstance("client", type = "MOOSClient", **kargs)
--- python/lib/morsel_moos/client/moos_client.py
+++ python/lib/morsel_moos/client/moos_client.py
@@ -4,10 +4,9 @@ from morsel_moos.moosc import MOOSClient as CMOOSClient
 #-------------------------------------------------------------------------------
 
 class MOOSClient(Node):
-  def __init__(self, name = "MorselMOOS", configFile = "",
-      serverHost = "localhost", serverPort = 9000, commTick = 10,
-      quiet = True, **kargs):
-    Node.__init__(self, name, **kargs)
+  def __init__(self, configFile = "", serverHost = "localhost",
+      serverPort = 9000, commTick = 10, quiet = True, **kargs):
+    super(MOOSClient, self).__init__(**kargs)
 
     self.configFile = configFile
     self.serverHost = serverHost
@@ -15,11 +14,11 @@ class MOOSClient(Node):
     self.commTick = commTick
     self.quiet = quiet
 
-    self.client = CMOOSClient(name, self.configFile, self.serverHost,
+    self.client = CMOOSClient("CMOOSClient", self.configFile, self.serverHost,
       self.serverPort, self.commTick, self.quiet)
     self.client.reparentTo(self)
 
-    framework.scheduler.addTask(name+"Receive", self.receive,
+    framework.scheduler.addTask(self.name+"/Receive", self.receive,
       1.0/self.client.getCommTick())
 
 #-------------------------------------------------------------------------------
